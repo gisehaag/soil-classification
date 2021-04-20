c32107d7e026acff637f05905175b67746fa3de6
Gise Haag <ing.giselahaag@gmail.com>
2021-04-20 17:08:43 +0200

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<header
+		class="flex items-center border-1 border-gray-200 pl-6 max-w-7xl text-2xl bg-white bg-opacity-100 shadow font-extrabold"
+	>
+		<h1 class="">header | Soil classification app</h1>
+	</header>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap');
+
+header {
+	font-family: 'Poppins', sans-serif;
+	height: var(--header-height);
+}
+</style>
