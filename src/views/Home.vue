a8f7dd00119f2173d6fb15a145da7ac6f6a3ffb4
Gise Haag <ing.giselahaag@gmail.com>
2021-04-20 17:09:36 +0200

    Add Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <theoric-mark></theoric-mark>
+</template>
+
+<script>
+import TheoricMark from '@/components/TheoricMark';
+
+export default {
+  name: 'Home',
+
+  components: { TheoricMark },
+};
+</script>
