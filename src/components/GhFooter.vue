6196e8847d9a975a98dbc24506834c3023409673
Gise Haag <ing.giselahaag@gmail.com>
2021-05-05 17:14:43 +0200

    Add id

--- src/components/GhFooter.vue
+++ src/components/GhFooter.vue
@@ -1,43 +1,43 @@
 <template>
-  <footer class="flex items-center justify-center">
-    <div class="footer-content">
-      <span>
-        <img
-          width="12px"
-          src="https://s.w.org/images/core/emoji/13.0.1/svg/270f.svg"
-          alt="✏"
-        />
-      </span>
-      <span>Developed with</span>
-      <span>
-        <img
-          width="12px"
-          src="https://s.w.org/images/core/emoji/13.0.1/svg/2764.svg"
-          alt="❤"
-        />
-      </span>
-      <span>by</span>
-      <a href="https://gisehaag.com" target="_blank">GisehaaG</a>
-      <span>
-        <img
-          width="12px"
-          src="https://s.w.org/images/core/emoji/13.0.1/svg/2b50.svg"
-          alt="⭐"
-        />
-      </span>
-      <span>{{ year }}</span>
-    </div>
-  </footer>
+	<footer class="flex items-center justify-center" id="footer">
+		<div class="footer-content">
+			<span>
+				<img
+					width="12px"
+					src="https://s.w.org/images/core/emoji/13.0.1/svg/270f.svg"
+					alt="✏"
+				/>
+			</span>
+			<span>Developed with</span>
+			<span>
+				<img
+					width="12px"
+					src="https://s.w.org/images/core/emoji/13.0.1/svg/2764.svg"
+					alt="❤"
+				/>
+			</span>
+			<span>by</span>
+			<a href="https://gisehaag.com" target="_blank">GisehaaG</a>
+			<span>
+				<img
+					width="12px"
+					src="https://s.w.org/images/core/emoji/13.0.1/svg/2b50.svg"
+					alt="⭐"
+				/>
+			</span>
+			<span>{{ year }}</span>
+		</div>
+	</footer>
 </template>
 
 <script>
 export default {
-  name: 'GhFooter',
-  data() {
-    return {
-      year: 2021,
-    };
-  },
+	name: 'GhFooter',
+	data() {
+		return {
+			year: 2021,
+		};
+	},
 };
 </script>
 
@@ -47,36 +47,36 @@ export default {
 $primary-color: #ffb400;
 
 footer {
-  font-family: 'Montserrat', sans-serif;
-  background-color: #fafafa;
-  width: 100%;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  z-index: 999;
-  height: var(--footer-height);
+	font-family: 'Montserrat', sans-serif;
+	background-color: #fafafa;
+	width: 100%;
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	z-index: 999;
+	height: var(--footer-height);
 
-  img {
-    display: inline-block;
-  }
+	img {
+		display: inline-block;
+	}
 
-  .footer-content {
-    font-size: 12px;
-    color: #464646;
-  }
+	.footer-content {
+		font-size: 12px;
+		color: #464646;
+	}
 
-  .footer-content a {
-    color: #333;
-    text-decoration: none;
+	.footer-content a {
+		color: #333;
+		text-decoration: none;
 
-    &:hover {
-      color: $primary-color;
-    }
-  }
+		&:hover {
+			color: $primary-color;
+		}
+	}
 
-  .footer-content span {
-    padding: 0 5px;
-    font-style: normal;
-  }
+	.footer-content span {
+		padding: 0 5px;
+		font-style: normal;
+	}
 }
 </style>
