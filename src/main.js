2e28d8b506ff7f7a462f62685aba6843f8686902
Gise Haag <ing.giselahaag@gmail.com>
2021-05-05 17:12:10 +0200

    Add Charkick pluggin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import '@/assets/css/tailwind.css';
+import Chartkick from 'vue-chartkick';
+import Chart from 'chart.js';
 
 import router from '@/router';
 
+Vue.use(Chartkick.use(Chart));
 Vue.config.productionTip = false;
 
 new Vue({
