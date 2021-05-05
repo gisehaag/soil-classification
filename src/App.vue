2ff4432f1f9d5b02f1222b22b76d7c3ca3cbf434
Gise Haag <ing.giselahaag@gmail.com>
2021-05-05 17:14:28 +0200

    Add print media query

--- src/App.vue
+++ src/App.vue
@@ -43,4 +43,56 @@ body {
 	color: #2a2a2a;
 	padding-bottom: var(--footer-height);
 }
+
+@media print {
+	#header,
+	.submit,
+	#footer,
+	.toggle-system,
+	.print {
+		display: none !important;
+	}
+
+	div.box {
+		display: block !important;
+	}
+
+	form,
+	#data {
+		width: 80vw !important;
+		margin: 0 auto !important;
+	}
+
+	canvas {
+		width: 100% !important;
+		height: auto !important;
+	}
+
+	form {
+		break-after: page;
+	}
+
+	.graphic-box.plasticidad {
+		width: 12cm !important;
+		height: 10.5cm !important;
+	}
+	.graphic-box.granulometria {
+		width: 18cm !important;
+		height: 10.5cm !important;
+	}
+
+	.results {
+		width: 80vw !important;
+	}
+
+	.consistencia {
+		flex-direction: column;
+	}
+
+	@page {
+		size: A4 portrait;
+		margin: 1cm;
+		// page-break-inside: avoid;
+	}
+}
 </style>
