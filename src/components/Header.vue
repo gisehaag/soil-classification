76d21105d43020753545098adcf3a856ba66785e
Gise Haag <ing.giselahaag@gmail.com>
2021-06-04 18:06:28 +0200

    Update header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,57 @@
 <template>
 	<header
-		class="flex items-center border-1 border-gray-200 pl-6 text-xl bg-white bg-opacity-100 shadow font-extrabold"
+		class="flex items-center border-1 border-gray-200 pl-6 text-xl bg-white bg-opacity-100 font-extrabold"
 		id="header"
 	>
 		<div>
 			<ul>
 				<li><router-link to="/">Inicio</router-link></li>
-				<li><router-link to="/about">Cómo funciona?</router-link></li>
+				<li>
+					<router-link to="/clasificar/sucs">SUCS</router-link>
+				</li>
+				<li>
+					<router-link to="/clasificar/aashto">AASHTO</router-link>
+				</li>
+				<li>
+					<router-link to="/about">Marco Teórico</router-link>
+				</li>
 			</ul>
 		</div>
 	</header>
 </template>
 
-<style scoped>
+
+<script>
+export default {
+	name: 'Header',
+
+	data() {
+		return {};
+	},
+
+	computed: {
+		isAbout() {
+			return this.$route.path == '/about' ? true : false;
+		},
+		isHome() {
+			return this.$route.path == '/' ? true : false;
+		},
+	},
+};
+</script>
+
+
+<style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap');
+
+$primary-color: #ffb400;
+
+#header {
+	border-bottom: 3px solid #dedede !important;
+	box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.4) !important;
+	/* -webkit-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.4) !important; */
+	/* -moz-box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.4) !important; */
+}
 
 header {
 	font-family: 'Poppins', sans-serif;
@@ -27,5 +65,9 @@ ul {
 
 li {
 	margin-right: 35px;
+}
+
+li:hover {
+	color: $primary-color;
 }
 </style>
