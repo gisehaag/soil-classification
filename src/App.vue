3baf6e4ce2bf5ad8eb5e779eb8fc617e29b09aaa
Gise Haag <ing.giselahaag@gmail.com>
2021-10-05 18:32:24 +0200

    Change title and main image

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 
 	data() {
 		return {
-			title: 'Soil classification app',
+			title: 'Clasificación de Suelos para uso ingenieril',
 		};
 	},
 };
@@ -32,7 +32,7 @@ $primary-color: #ffb400;
 :root {
 	--footer-height: 50px;
 	--header-height: 85px;
-	--background-img: url('https://images.unsplash.com/photo-1463099843411-206f505c28bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTIyNDV8MHwxfGFsbHwyOXx8fHx8fDJ8fDE2MTg1Njc2NDQ&ixlib=rb-1.2.1&q=80&w=1080');
+	--background-img: url('https://images.unsplash.com/photo-1517089596392-fb9a9033e05b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTIyNDV8MHwxfHNlYXJjaHwxNHx8YnVpbGRpbmclMjBsb2FkZXJ8ZW58MHx8fHwxNjMzMzM3NzIw&ixlib=rb-1.2.1&q=80&w=1080');
 }
 
 body {
